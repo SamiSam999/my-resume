90c718b4375ee1ac6103d4d246aca6dd04fe9611
samisam999 <mnsa999@yahoo.com>
2021-08-24 16:14:48 +0300

    my resume

---
+++ cv.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+    
+function ar(){
+    document.documentElement.dir = "rtl";
+var $ar = document.getElementById("1").innerHTML = "<h1 style=margin-right:45px>مــحــمــــد نـــــــور الســـــــحار</h1>";
+ $ar = document.getElementById("2").innerHTML = "مـــطـــور بــرمـجـــيــــات";
+$ar = document.getElementById("3").innerHTML =  '<h3 >الأنجازات والتكريم</h3>';
+$ar = document.getElementById("4").innerHTML =" <p >  عضو فريق الجامعة البرمجي للطلبة المتفوقين. شاركت بمسابقة (ألتقط العلم) ضمن فريق (دارك ويب)التي نظمتها منصة (سايبر هب) تحت مظلة الاتحاد السعودي للأمن السيبراني والبرمجة والدرونز لدعم قدرات الطلاب الجامعيين وفزنا بالمركز الأول في المملكة بالنسبة للجامعات الخاصةوالمركز الحادي عشر في المملكة العربية السعودية بالنسبة للجامعات ككل.   تم تقيم مشروعي من ضمن أفضل مشاريع التخرج وتكريمي من قبل الجامعة العربية المفتوحة للمشاركة بمسابقة المشاريع الأولى</p>";
+$ar = document.getElementById("21").innerHTML ="<span >: الخبر,السعودية</span>";
+$ar = document.getElementById("22").innerHTML ="<span>الجنسية: سوري</span>";
+$ar = document.getElementById("25").innerHTML ="<h3 style=color:blue;> المهارات</h3> <ul>  <li >Java</li> <li >Java FX</li> <li >JavaScript</li> <li >HTML</li> <li >CSS</li> <li >SQL</li> <li >Bootstarp</li> <li >Spring Framework.</li> <li >MicroSoft Office.</li> <li >معرفة بالشبكات.</li> <li >خلفية باختبار الأختراق.</li> <li >التواصل ضمن فريق العمل.</li> <li >حل المشكلات وتطوير الذات. </li> <li > اجادة اللغة العربية والأنكليزية. </li> </ul> </ul>";
+
+$ar = document.getElementById("11").innerHTML ="<h3>التعليم</h3>";
+$ar = document.getElementById("12").innerHTML ="<p >درجة البكالوريوس في العلوم تقنية معلومات وحوسبة الجامعة العربية المفتوحة/الجامعة البريطانية المفتوحة بتقدير ممتاز/ مرتبة الشرف من الدرجة الأولى 2016-2020<br/></p>";
+$ar = document.getElementById("13").innerHTML ="<h3 >المشاريع</h3> ";
+$ar = document.getElementById("14").innerHTML ="<spane >نموذج التشفير التوقيعي:</spane> <br/>";
+$ar = document.getElementById("15").innerHTML ="<span >تطبيق مكتبي جافا (يستخدم لأغراض الأمن السيبراني)</span> <br/>";
+$ar = document.getElementById("16").innerHTML ="<spane > الضريبة السهلة: </spane><br/>";
+$ar = document.getElementById("17").innerHTML ="<span >تطبيق مكتبي جافا(لحساب النسبة والقيمة المضافة)</span> <br>";
+$ar = document.getElementById("18").innerHTML ="<spane >السيرة الذاتية: </spane><br/>";
+$ar = document.getElementById("19").innerHTML ="<span >موقع الكتروني للسيرتي الذاتية</span>";
+
+}
+function en(){
+    document.documentElement.dir = "ltr";
+    var $en = document.getElementById("1").innerHTML = "<h1>Mohammed Nour Al Sahhar</h1>";
+ $en = document.getElementById("2").innerHTML = "SOFTWARE DEVELOPER";
+$en = document.getElementById("3").innerHTML =  '<h3 >ABOUT ME</h3>';
+$en = document.getElementById("4").innerHTML =" <p >I was part of first place winning team for private universities in the CTF competition hosted for privateand public universities in Saudi Arabia.<br/> Our team participator in competition of the Saudi Federation for Cyber security Programming and Drones. We won place 11th out of over 300 participant teams.<br/>I am most proud of my Arab Open University  Appreciation Award that was given to me for my participation of first projects competition.<br/> finally i was a member of programming team of the University for top students.</p>";
+$en = document.getElementById("21").innerHTML ="<span >Al Khobar, Saudi Arabia</span>";
+$en = document.getElementById("22").innerHTML ="<span>Nationality: Syrian</span>";
+$en = document.getElementById("25").innerHTML ="<h3 style=color:blue;> Skills</h3>  <ul> <p><li >Java</li><li>Java FX</li><li >JavaScript</li><li >HTML</li><li >CSS</li><li >SQL</li><li >PHP</li><li >Bootstarp</li><li >Spring Framework.</li><li >MicroSoft Office.</li><li>Good background in networking.</li><li >Good background in penetration testing.</li><li >Good teamwork communication.</li><li >Good problem solving. </li><li > Fluent in English and Arabic. </li> </p></ul>";
+
+$en = document.getElementById("11").innerHTML ="<h3>EDUCATION</h3>";
+$en = document.getElementById("12").innerHTML ="<p > Bachelor’s Degree of Science Information Technology and Computing Arab Open University/ British Open University GPA 3.63 with First Class Honours. 2016-2020 <br/></p>";
+$en = document.getElementById("13").innerHTML ="<h3 >PROJECTS EXPERINCE</h3> ";
+$en = document.getElementById("14").innerHTML ="<spane >Signcryption Scheme:</spane> <br/>";
+$en = document.getElementById("15").innerHTML ="<span >Desktop Application (used for cyber security reasons).</span> <br/>";
+$en = document.getElementById("16").innerHTML ="<spane >Easy Tax:</spane><br/>";
+$en = document.getElementById("17").innerHTML ="<span >Desktop Application(calculate the VAT).</span> <br>";
+$en = document.getElementById("18").innerHTML ="<spane >My CV: </spane><br/>";
+$en = document.getElementById("19").innerHTML ="<span >website for my resume.</span>";
+
+}
