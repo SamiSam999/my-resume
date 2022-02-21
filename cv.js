4f7b6f91000690c667a8e0d87f25ca257eaddf88
SamiSam999 <89392385+SamiSam999@users.noreply.github.com>
2022-02-21 14:54:59 +0300

    Update cv.js

--- cv.js
+++ cv.js
@@ -7,7 +7,7 @@
 function ar(){
     document.documentElement.dir = "rtl";
 var $ar = document.getElementById("1").innerHTML = "<h1 style=margin-right:45px>مــحــمــــد نـــــــور الســـــــحار</h1>";
- $ar = document.getElementById("2").innerHTML = "مـــطـــور بــرمـجـــيــــات";
+ $ar = document.getElementById("2").innerHTML = "أخصائي تقنية معلومات";
 $ar = document.getElementById("3").innerHTML =  '<h3 >الأنجازات والتكريم</h3>';
 $ar = document.getElementById("4").innerHTML =" <p >  عضو فريق الجامعة البرمجي للطلبة المتفوقين. شاركت بمسابقة (ألتقط العلم) ضمن فريق (دارك ويب)التي نظمتها منصة (سايبر هب) تحت مظلة الاتحاد السعودي للأمن السيبراني والبرمجة والدرونز لدعم قدرات الطلاب الجامعيين وفزنا بالمركز الأول في المملكة بالنسبة للجامعات الخاصةوالمركز الحادي عشر في المملكة العربية السعودية بالنسبة للجامعات ككل.   تم تقيم مشروعي من ضمن أفضل مشاريع التخرج وتكريمي من قبل الجامعة العربية المفتوحة للمشاركة بمسابقة المشاريع الأولى</p>";
 $ar = document.getElementById("21").innerHTML ="<span >: الخبر,السعودية</span>";
@@ -29,7 +29,7 @@ $ar = document.getElementById("19").innerHTML ="<span >موقع الكتروني
 function en(){
     document.documentElement.dir = "ltr";
     var $en = document.getElementById("1").innerHTML = "<h1>Mohammed Nour Al Sahhar</h1>";
- $en = document.getElementById("2").innerHTML = "SOFTWARE DEVELOPER";
+ $en = document.getElementById("2").innerHTML = "IT Specialist";
 $en = document.getElementById("3").innerHTML =  '<h3 >ABOUT ME</h3>';
 $en = document.getElementById("4").innerHTML =" <p >I was part of first place winning team for private universities in the CTF competition hosted for privateand public universities in Saudi Arabia.<br/> Our team participator in competition of the Saudi Federation for Cyber security Programming and Drones. We won place 11th out of over 300 participant teams.<br/>I am most proud of my Arab Open University  Appreciation Award that was given to me for my participation of first projects competition.<br/> finally i was a member of programming team of the University for top students.</p>";
 $en = document.getElementById("21").innerHTML ="<span >Al Khobar, Saudi Arabia</span>";
@@ -39,7 +39,7 @@ $en = document.getElementById("25").innerHTML ="<h3 style=color:blue;> Skills</h
 $en = document.getElementById("11").innerHTML ="<h3>EDUCATION</h3>";
 $en = document.getElementById("12").innerHTML ="<p > Bachelor’s Degree of Science Information Technology and Computing Arab Open University/ British Open University GPA 3.63 with First Class Honours. 2016-2020 <br/></p>";
 $en = document.getElementById("13").innerHTML ="<h3 >EXPERINCE</h3> ";
-$ar = document.getElementById("20").innerHTML ="";
+$ar = document.getElementById("20").innerHTML =" <span> IT specialist (Compliance Assessment, System Administrator)<br/>Mohammed Aldossary Sons Co. <br/> 01/2021-current. Saudi Arabia-Khobar</span> <br/>";
 $en = document.getElementById("14").innerHTML ="<spane >Signcryption Scheme:</spane> <br/>";
 $en = document.getElementById("15").innerHTML ="<span >Desktop Application (used for cyber security reasons).</span> <br/>";
 $en = document.getElementById("16").innerHTML ="<spane >Easy Tax:</spane><br/>";
