b62a7f8e81b2e92a3bf8bda549105921bad96829
SamiSam999 <89392385+SamiSam999@users.noreply.github.com>
2023-01-29 18:38:18 +0300

    Update cv.js

--- cv.js
+++ cv.js
@@ -1,12 +1,12 @@
 function ar(){
     document.documentElement.dir = "rtl";
 var $ar = document.getElementById("1").innerHTML = "<h1 style=margin-right:45px>مــحــمــــد نـــــــور الســـــــحار</h1>";
- $ar = document.getElementById("2").innerHTML = "أخصائي تقنية معلومات";
+ $ar = document.getElementById("2").innerHTML = "مطور جافا";
 $ar = document.getElementById("3").innerHTML =  '<h3 >الأنجازات والتكريم</h3>';
 $ar = document.getElementById("4").innerHTML =" <p >  عضو فريق الجامعة البرمجي للطلبة المتفوقين. شاركت بمسابقة (ألتقط العلم) ضمن فريق (دارك ويب)التي نظمتها منصة (سايبر هب) تحت مظلة الاتحاد السعودي للأمن السيبراني والبرمجة والدرونز لدعم قدرات الطلاب الجامعيين وفزنا بالمركز الأول في المملكة بالنسبة للجامعات الخاصةوالمركز الحادي عشر في المملكة العربية السعودية بالنسبة للجامعات ككل.   تم تقيم مشروعي من ضمن أفضل مشاريع التخرج وتكريمي من قبل الجامعة العربية المفتوحة للمشاركة بمسابقة المشاريع الأولى</p>";
 $ar = document.getElementById("21").innerHTML ="<span >: الخبر,السعودية</span>";
 $ar = document.getElementById("22").innerHTML ="<span>الجنسية: سوري</span>";
-$ar = document.getElementById("25").innerHTML ="<h3 style=color:blue;> المهارات</h3> <ul>  <li >Java</li> <li >Java FX</li> <li >JavaScript</li> <li >HTML5</li> <li >CSS</li> <li >SQL Server</li> <li >Bootstarp</li> <li >WINDOWS SERVER</li><li >MICROSOFT 365</li> <li >MicroSoft Office.</li> <li >معرفة بالشبكات.</li>  <li >التواصل ضمن فريق العمل.</li> <li >حل المشكلات وتطوير الذات. </li> <li > اجادة اللغة العربية والأنكليزية. </li> </ul> </ul>";
+$ar = document.getElementById("25").innerHTML ="<h3 style=color:blue;> المهارات</h3> <ul>  <li >Java</li> <li >Java FX</li> <li >JavaScript</li> <li >HTML5</li> <li >CSS</li> <li >SQL Server</li> <li >Bootstarp</li>  <li >معرفة بالشبكات.</li> <li >حل المشكلات وتطوير الذات. </li> <li > اجادة اللغة العربية والأنكليزية. </li> </ul> </ul>";
 
 $ar = document.getElementById("11").innerHTML ="<h3>التعليم</h3>";
 $ar = document.getElementById("12").innerHTML ="<p >درجة البكالوريوس في العلوم تقنية معلومات وحوسبة الجامعة العربية المفتوحة/الجامعة البريطانية المفتوحة بتقدير ممتاز/ مرتبة الشرف من الدرجة الأولى 2016-2020<br/></p>";
